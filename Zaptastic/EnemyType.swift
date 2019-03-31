25f245a189a8887622a8901ebe3048f7b89e3ce1
Michele Galvagno <michicello@gmail.com>
2019-03-31 21:20:24 +0200

    First commit to fetch an error...

---
+++ Zaptastic/EnemyType.swift
@@ -0,0 +1,16 @@
+//
+//  EnemyType.swift
+//  Zaptastic
+//
+//  Created by Michele Galvagno on 31/03/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import SpriteKit
+
+struct EnemyType: Codable {
+    let name: String
+    let shields: Int
+    let speed: CGFloat
+    let powerUpChange: Int
+}
